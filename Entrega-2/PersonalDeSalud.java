386ffa0e12434e9aeb7dbb2184fec08272530f89
JOAQUIN GROSSO <joagrosso@hotmail.com>
2025-05-14 10:18:50 -0300

    Delete PersonalDeSalud.java

--- Entrega-2/PersonalDeSalud.java
+++
@@ -1,7 +0,0 @@
-
-public class PersonalDeSalud extends Usuario{
-
-	public PersonalDeSalud(String nombreUsuario, String email, String contraseña) {
-		super(nombreUsuario, email, contraseña, RolUsuario.PERSONALDESALUD);
-	}
-}
