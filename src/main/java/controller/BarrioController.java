6213b4d9ac07b677390625d553c3af90f8bc8985
joagrosso <joagrosso@hotmail.com>
2025-06-18 03:46:38 -0300

    fin abml y testeos de barrio

--- src/main/java/controller/BarrioController.java
+++ src/main/java/controller/BarrioController.java
@@ -123,19 +123,13 @@ public class BarrioController {
 
     @DELETE
     @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
     @Operation(description = "Este endpoint nos permite eliminar el barrio a partir de un id",
             parameters = @Parameter(name = "barrio id"))
     public Response delete(@PathParam("id") Long id) {
-        Optional<Barrio> objeto = barrioService.buscarPorId(id);
-        if (objeto.isPresent()) {
-            barrioService.eliminar(objeto.get());
-            return Response.noContent().build();
-        } else {
-            return Response.status(Response.Status.NOT_FOUND).entity(Map.of(
-                    "error", "Barrio no encontrado",
-                    "code", 404
-            )).build();
-        }
+        barrioService.eliminar(id);
+        return Response.noContent().build();
+
     }
 }
 
