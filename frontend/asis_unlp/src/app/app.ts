c3ebd20c791742e4015d274665223286bb650cff
matiasc <matiasc@pagotic.com>
2025-07-12 18:20:19 -0300

    Agrego header para navegacion con logica breadcrumbs para navegar

--- frontend/asis_unlp/src/app/app.ts
+++ frontend/asis_unlp/src/app/app.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule, HeaderComponent],
   templateUrl: './app.html',
   styles: []
 })
