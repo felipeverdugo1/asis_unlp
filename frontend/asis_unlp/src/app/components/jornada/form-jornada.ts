a85fc9821db2d2995a393c4e406649944f878726
felipeverdugo <felipeverdugo016@gmail.com>
2025-07-15 20:51:20 -0300

    administrar-zonas

--- frontend/asis_unlp/src/app/components/jornada/form-jornada.ts
+++ frontend/asis_unlp/src/app/components/jornada/form-jornada.ts
@@ -2,6 +2,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Jornada } from '../../models/jornada.model';
 import { NgModel, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Zona } from '../../models/zona.model';
+
 
 @Component({
   standalone: true,
@@ -25,4 +27,26 @@ export class FormJornada {
 export class FormActualizarJornada {
   @Input() jornada: Jornada = { id: 0, fechaInicio: '', fechaFin: '', campaña_id: 0 };
   @Output() onSubmit = new EventEmitter<Jornada>();
+}
+
+
+@Component({
+  standalone: true,
+  selector: 'form-administrar-zonas',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './form-administrar-zonas.html',
+  styleUrls: ['../../../styles.css']
+})
+
+export class FormAdministrarZonas {
+  @Input() zonas: (Zona & { seleccionada?: boolean })[] = [];
+  @Output() onSubmit = new EventEmitter<(Zona & { seleccionada?: boolean })[]>();
+
+  guardar() {
+    this.onSubmit.emit(this.zonas);
+  }
+
+  zonasSeleccionadas() {
+    return this.zonas.filter(z => z.seleccionada);
+  }
 }
